077f4c272fcaf5844bd1e87ca868640118ceeae9
shpeeck <jenya_vas@mail.ru>
2021-07-29 22:52:59 +0300

    version 2

---
+++ js/script.js
@@ -0,0 +1,111 @@
+
+  window.onload = function () {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+      document.body.classList.add('loaded')
+      document.body.classList.remove('loaded_hiding');
+    }, 500);
+  }
+
+
+const app = {
+    data() {
+        return {
+            start: new Date(),
+            end: new Date(),
+            step: 0,
+            downtime: 0,
+            details: 1,
+            result: 0,
+            selected: 'Eng',
+            text: {
+                start: 'Start time',
+                end: 'End time',
+                step: 'Cycle (sec)',
+                downtime: 'Downtime (min)',
+                details: 'Parts in a cycle',
+                totall: 'Totall details: '
+            },
+            eng: {
+                start: 'Start time',
+                end: 'End time',
+                step: 'Cycle (sec)',
+                downtime: 'Downtime (min)',
+                details: 'Parts in a cycle',
+                totall: 'Totall details: '
+            },
+            ua: {
+                start: 'Час старту',
+                end: 'Час закінчення',
+                step: 'Цикл (сек)',
+                downtime: 'Простой (хвл)',
+                details: 'Деталей в циклі',
+                totall: 'Всього деталей: '
+            },
+            ru: {
+                start: 'Время начала',
+                end: 'Время окончания',
+                step: 'Цикл (сек)',
+                downtime: 'Простой (мин)',
+                details: 'Детлей в цикле',
+                totall: 'Всего деталей: '
+            },
+            pl: {
+                start: 'Start godzina',
+                end: 'Stop godzina',
+                step: 'Cykl (sek)',
+                downtime: 'Postoj (min)',
+                details: 'Czesci w cyklu',
+                totall: 'Calkowity: '
+            }
+        }
+    },
+        methods: {
+            leng() {
+                if(this.selected == 'Eng') {
+                    this.text = this.eng;
+                    // return this.eng;
+                }else if (this.selected == 'Ua') {
+                    this.text = this.ua;
+                }else if (this.selected == 'Ru') {
+                    this.text = this.ru;
+                }else if (this.selected == 'Pl') {
+                    this.text = this.pl;
+                }else {
+                    this.text = ''
+                }
+            },
+            startTime(event) {
+                this.start = event.target.value;
+                this.res()
+            },
+            endTime(event) {
+                this.end = event.target.value;
+            },
+            stepTime(event) {
+                this.step = event.target.value;
+                this.res()
+            },
+            downtimeTime(event) {
+                this.downtime = event.target.value;
+                this.res()
+            },
+            detailsCol(event) {
+                this.details = event.target.value;
+                this.res()
+            },
+            res() {
+                    let a = this.end,
+                    b = this.start
+                    c = (Math.floor((((new Date(a).getTime() - new Date(b).getTime()) / 1000) - (this.downtime * 60)) / this.step)) * this.details;
+                    if (c > 100000000000000) {
+                        this.result = "not all fields";
+                    }else {
+                        this.result = c;
+                    }
+
+            }
+        }
+    }
+
+        Vue.createApp(app).mount('.app')
